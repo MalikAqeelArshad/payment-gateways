ff6496dd8c977124fc6ee9c9a01104120bf41843
Aqeel Malik <malik.aqeelarshad@gmail.com>
2024-06-24 09:16:56 +0400

    PayPal payment gateway integration

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,14 @@
 <?php
 
+use App\Models\Payment;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PaypalController;
 
+Route::get('/', function () {
+    return dd(Payment::all()->toArray());
+});
 Route::view('/', 'welcome');
+
+Route::post('paypal', [PaypalController::class, 'paypal'])->name('paypal');
+Route::get('success', [PaypalController::class, 'success'])->name('success');
+Route::get('cancel', [PaypalController::class, 'cancel'])->name('cancel');
